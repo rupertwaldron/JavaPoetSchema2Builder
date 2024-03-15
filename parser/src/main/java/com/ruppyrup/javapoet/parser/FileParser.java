52cc7ee7ecd5bc56c677a9786d191e49986529ef
rupertwaldron <rupert.waldron@yahoo.co.uk>
2024-03-15 18:00:36 +0000

    Added parser test

--- parser/src/main/java/com/ruppyrup/javapoet/parser/FileParser.java
+++ parser/src/main/java/com/ruppyrup/javapoet/parser/FileParser.java
@@ -5,14 +5,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ruppyrup.javapoet.app.PoetParser;
 import org.apache.commons.io.IOUtils;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 public class FileParser implements PoetParser {
 
     public JsonNode parse(String fileName) throws IOException {
-        FileInputStream fisTargetFile = new FileInputStream("src/main/resources/nested_schema.json");
+        System.out.println(new File(".").getAbsolutePath());
+
+        FileInputStream fisTargetFile = new FileInputStream(fileName);
 
         String targetFileStr = IOUtils.toString(fisTargetFile, StandardCharsets.UTF_8);
 
--- parser/src/test/java/com/ruppyrup/javapoet/parser/FileParserTest.java
+++ parser/src/test/java/com/ruppyrup/javapoet/parser/FileParserTest.java
@@ -1,19 +1,26 @@
 package com.ruppyrup.javapoet.parser;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 class FileParserTest {
 
     @Test
     void parse() throws IOException {
         FileParser fileParser = new FileParser();
-        JsonNode jsonNode = fileParser.parse("parser/src/test/resources/nested_schema.json");
-        assertNotNull(jsonNode);
-//        jsonNode.path()
+        JsonNode jsonNode = fileParser.parse("src/test/resources/nested_schema.json");
+
+        FileInputStream expectedIS = new FileInputStream("src/test/resources/expected_output.json");
+        String expectedString = IOUtils.toString(expectedIS, StandardCharsets.UTF_8);
+
+        assertThat(jsonNode.toString()).isEqualTo(expectedString);
     }
 }

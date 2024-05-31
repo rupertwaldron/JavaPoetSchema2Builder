9254262298ce1f69eba1be44adc422890322205c
rupertwaldron <rupert.waldron@yahoo.co.uk>
2024-05-31 18:37:43 +0100

    Updated book getter

--- maker/src/main/java/com/ruppyrup/javapoet/maker/factories/GetterMethodFactory.java
+++ maker/src/main/java/com/ruppyrup/javapoet/maker/factories/GetterMethodFactory.java
@@ -1,12 +1,15 @@
 package com.ruppyrup.javapoet.maker.factories;
 
+import com.ruppyrup.javapoet.app.FieldType;
 import com.ruppyrup.javapoet.app.SchemaField;
 import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterizedTypeName;
 import com.squareup.javapoet.TypeName;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.lang.model.element.Modifier;
+import java.util.List;
 
 public class GetterMethodFactory {
     public static MethodSpec.Builder getGetterMethod(SchemaField<?> schemaField) {
@@ -14,10 +17,16 @@ public class GetterMethodFactory {
         MethodSpec.Builder builder = MethodSpec.methodBuilder("get" + className)
                 .addModifiers(Modifier.PUBLIC);
 
-        if (schemaField.clazz().getName().equals("java.lang.Object")) {
+        if (schemaField.clazz().getName().equals(FieldType.OBJECT.typeIdentifier)) {
             TypeName childTypeName = ClassName.get("", className);
             builder.addStatement("return this.$N", schemaField.name())
                     .returns(childTypeName);
+        } else if (schemaField.clazz().getName().equals(FieldType.OBJECT_ARRAY.typeIdentifier)) {
+            TypeName childTypeName = ClassName.get("", StringUtils.chop(className));
+            ClassName list = ClassName.get(List.class);
+            ParameterizedTypeName listOfItems = ParameterizedTypeName.get(list, childTypeName);
+            builder.addStatement("return this.$N", schemaField.name())
+                    .returns(listOfItems);
         } else {
             builder.addStatement("return this.$N", schemaField.name())
                     .returns(schemaField.clazz());
